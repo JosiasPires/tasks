a6dd46f0c409a63572e2ec11311f75e340301864
JosiasPires <104743492+JosiasPires@users.noreply.github.com>
2025-05-01 01:19:10 -0300

    Create render module

---
+++ src/render.js
@@ -0,0 +1,15 @@
+const sidebar = document.querySelector("#sidebar");
+
+function renderProject(project) {
+    const container = document.createElement('div');
+    const title = document.createElement('h3');
+    const removeBtn = document.createElement('button');
+
+    title.textContent = project.name;
+    removeBtn.addEventListener('click', (e) => {
+        project.remove();
+    })
+    
+    container.appendChild(title);
+    container.appendChild(removeBtn);
+}
