32e8146a6ea8fa5b0de433aa3142a36e053135ee
JosiasPires <104743492+JosiasPires@users.noreply.github.com>
2025-04-30 23:24:08 -0300

    Add delete feature to do lists

--- src/project.js
+++ src/project.js
@@ -1,4 +1,12 @@
 function createProject(name) {
     const toDoList = [];
+
+    toDoList.forEach((item, index) => {
+        item.delete = () => {
+            toDoList.splice(index, 1);
+        }
+    })
+    
     return {name, toDoList};
 }
+
