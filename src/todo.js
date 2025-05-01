04f88d60f2f6c292365f8736043e2574888a8ff2
JosiasPires <104743492+JosiasPires@users.noreply.github.com>
2025-04-30 22:45:50 -0300

    create to-do module

---
+++ src/todo.js
@@ -0,0 +1,13 @@
+class ToDo {
+    constructor(title, description='', dueDate, priority=0) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.isDone = false;
+    }
+    check() {
+        this.isDone = !this.isDone;
+    }
+
+}
