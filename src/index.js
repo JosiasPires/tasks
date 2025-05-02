db6c69d025e453593f7f2462e51f0b2ed2b4433d
JosiasPires <104743492+JosiasPires@users.noreply.github.com>
2025-05-02 01:16:24 -0300

    Import render modules

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 import { ToDo } from "./todo.js";
 import { createProject } from "./project.js";
 import { createProjectManager } from "./projectsManager.js";
-import { renderProject, renderToDo } from "./render.js";
+import { renderModal, renderProject, renderToDo } from "./render.js";
 
